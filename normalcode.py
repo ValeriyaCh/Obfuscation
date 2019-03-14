1ef0fa1033b45817ed80e9473bf7bccca10878ac
rukubrakov <48324827+rukubrakov@users.noreply.github.com>
2019-03-14 17:08:39 +0300

    Update normalcode.py

--- normalcode.py
+++ normalcode.py
@@ -1,12 +1,1 @@
-def lcm(a, b):
-    m = a * b
-    while a != 0 and b != 0:
-        if a > b:
-            a %= b
-        else:
-            b %= a
-    return m // (a + b)
 
-x = int(input('a='))
-y = int(input('b='))
-print('НОК:', lcm(x, y))
